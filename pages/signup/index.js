a09c5d87db3460d2364d441f68d345d482e8a647
Anand kushwaha <anandkushwaha70@gmail.com>
2022-08-21 03:21:06 +0530

    reels

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import TextField from '@mui/material/TextField';
+import Image from 'next/image';
+import logo from '../../assets/instagram.jpeg'
 
 function index() {
   return (
     <div className='signup-container'>
         <div className='signup-card'>
+            <Image src={logo}/>
         <TextField id="outlined-basic" size="small"  label="Email" variant="outlined" fullWidth margin='dense'/>
 
         <TextField id="outlined-basic" size="small"  label="Password" variant="outlined" fullWidth type="password" margin='dense'/>
