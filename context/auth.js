ee81331e99888e412b2be1324746193bedbf1e57
Anand kushwaha <anandkushwaha70@gmail.com>
2022-09-02 12:13:34 +0530

    auth

--- context/auth.js
+++ context/auth.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import {auth} from "../firebase";
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
-function AuthWapper() {
-    console.log("hello in auth wrapper")
+export const AuthContext =React.createContext();
+
+function AuthWapper({children}) {
+    console.log("hello in auth wrapper");
+
+    function login(email,password)
+    {
+        return signInWithEmailAndPassword(auth, email,password);
+    }
+
+    const store={
+        login
+    }
   return (
-    <div>hello</div>
-  )
+    <AuthContext.Provider value ={store}>
+       {children}
+    </AuthContext.Provider>
+   
+  );
 }
 
-export default AuthWapper
+export default AuthWapper;
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React ,{useContext} from 'react'
 import TextField from "@mui/material/TextField";
 import Image from "next/image"
 import logo from '../../assets/Instagram.jpeg'
 import Button from "@mui/material/Button";
 import Link from 'next/link'
 import { Carousel } from 'react-responsive-carousel';
+import { AuthContext } from '../../context/auth';
 function index()
 {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [error, setError] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+  
+  const { login } = useContext(AuthContext);
+
+  let handleClick = async() =>{
+    try{
+      console.log(email);
+      console.log(password);
+      setLoading(true);
+      setError('');
+      await login(email, password);
+      console.log("logged in");
+    }
+    catch(err)
+    {
+      console.log("error", JSON.stringify(err));
+      setError(err.message);
+    }
+
+    setLoading(false);
+  }
 return (
   <div className="login-container">
     <div className="insta-mob-bg">
@@ -34,6 +59,8 @@ return (
           variant="outlined"
           fullWidth
           margin="dense"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <TextField
           id="outlined-basic"
@@ -43,14 +70,25 @@ return (
           fullWidth
           margin="dense"
           type="password"
+          value={password}
+          onChange={(e)=> setPassword(e.target.value)}
         />
-           <span style={{ color: "blueviolet" }}>Forget Password</span>
+
+            {/* if error is present then show error */}
+          {error != "" && <div style={{ color: "red" }}>{error}</div>}
+          <Link href="/forgot">
+            <div style={{ color: "blue", marginTop: "0.5rem" }}>
+              Forget Password{" "}
+            </div>
+          </Link>
 
         <Button
           style={{ marginTop: "1rem" }}
           variant="contained"
           component="label"
           fullWidth
+          onClick={handleClick}
+          disabled={loading}
         >
           Log in
         </Button>
